8a418fe0a74adbee71629f61f453733adee8586d
Kamen Kirov <kamen@bgtown.com>
2024-07-30 08:10:31 +0000

    Installed summernote and created a post in the admin.

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-kamenco-blog-2an49o3y8k4.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
