74f15e5fb476808973855761d3e4279c22186432
Kamen Kirov <kamen@bgtown.com>
2024-08-07 20:12:02 +0000

    Added: Authentication

---
+++ templates/workspace/.vscode-remote/data/User/History/62a82f9e/3PI9.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://upnxcgjrbe3:thcQPU910yo3@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/ranch_mom_sulk_574769")
---
+++ templates/workspace/.vscode-remote/data/User/History/62a82f9e/IBlM.py
@@ -0,0 +1,8 @@
+
+
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://upnxcgjrbe3:thcQPU910yo3@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/ranch_mom_sulk_574769")
+    
+os.environ.setdefault("SECRET_KEY", "0c+($o@&9-fg+-(k@*@!j1b(e9uxvm4s5h_km$!97xn@gre^kt")
