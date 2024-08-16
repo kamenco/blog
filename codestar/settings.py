56ef6738315da13062d4d2dedaa70e7dc165cfab
Kamen Kirov <kamen@bgtown.com>
2024-08-16 20:00:05 +0000

    Secret key added.

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '8000-kamenco-blog-2an49o3y8k4.ws.codeinstitute-ide.net', '.herokuapp.com']
 
